8076d7aa3f53b10204d281fe6d36b2627e26cd6c
Anastasiya <nastja_1998-12@mail.ru>
2022-01-12 23:18:59 +0300

    Done tasks

---
+++ js-task_3/index.js
@@ -0,0 +1,60 @@
+//Задача 1
+for (i=1; i<=50; i++){
+    console.log(i);
+}
+
+for (i=35; i>=8; i--){
+    console.log(i);
+}
+
+//Задача 2
+i=89;
+while (i>=11) {
+	document.write(i + '<br>');
+	i--;
+}
+
+//Задача 3
+let sum = 0;
+for (i=0; i<=100; i++){
+    sum = sum + i;
+}
+console.log(sum);
+
+//Задача 4
+sum =0;
+for (i=1; i<=5; i++){
+    sum = sum + i;
+    console.log("В числе " + i + " сумма чисел равна " + sum);
+}
+
+//Задача 5
+for (i=8; i<=56; i++){
+    if (i % 2 !=0) continue;
+    console.log(i);
+}
+
+//Задача 6
+for (i=2; i<=10; i++){
+
+    for (j=2; j<=10; j++){
+    console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
+//Задача 7
+let n = 1000;
+for (i = 1; n > 50; n = n / 2, i++){
+    console.log(n + ", итерация - " + i);
+}
+
+//Задача 8
+
+
+//Задача 9
+
+
+
+//Задача 10
+
+
