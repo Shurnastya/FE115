0dadb5a10b287ae38febfb5a4c401ac264d5e037
Anastasiya <78867401+Shurnastya@users.noreply.github.com>
2022-02-28 19:13:51 +0300

    HW_28.02

--- js-task_8/index.js
+++ js-task_8/index.js
@@ -8,10 +8,11 @@ const Car = function(number, brand, engine){
         console.log(`
         Number: ${this.number}, 
         Brand: ${this.brand}, 
-        Engine: ${this.engine}`)
-    }
+        Engine: ${this.engine}
+        `);
+    };
 
-    this.get = function () {
+    this.get = function() {
 
         if (confirm('Включить двигатель?')) {
             this.way = +prompt('Сколько пройдено км?');
@@ -23,20 +24,39 @@ const Car = function(number, brand, engine){
         this.operation();
     };
 
-    this.operation = function () {
+    this.operation = function() {
         this.result = (this.way / 10) * this.costEnergy; //10 литров на 100км
          
         this.show();
     };
          
-    this.show = function () {
+    this.show = function() {
          
-        if (this.way == 0 || this.way == undefined) {
+        if (this.way === 0 || this.way === undefined) {
             console.log('Автомобиль "' + this.brand + '" под номером - ' + this.number + ' не включен.');
         } else {
             console.log('Автомобиль "' + this.brand + '" под номером - ' + this.number + ' проехал: ' + this.way + ' км, и стоимость топлива составило ' + this.result + ' BYN.');
-        }
+        };
     };
 };
 
-let car1 = new Car ('1015EL-3', 'Audi', 'Petrol');
+Car('7552IL-5', 'VW', 'Petrol');
+
+const car1 = new Car ('1015EL-3', 'Audi', 'Petrol');
+
+car1.showName();
+car1.get();
+car1.operation();
+car1.show();
+
+
+const CarBmw = function(options){
+    Car.apply(this, arguments)
+    this.type = 'sedan';
+    this.color = 'white';
+};
+
+CarBmw.prototype = Object.create(Car.prototype);
+
+const car2 = new CarBmw ('6615EL-4', 'BMW', 'Petrol');
+console.log(car2);
